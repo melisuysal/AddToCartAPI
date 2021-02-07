4d47297137234747956374948dc166ffcc80c60b
Melis Uysal <melis.uysal@softtech.isbank.com.tr>
2021-02-07 18:17:04 +0300

    Add project files.

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AddToCart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddToCart.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            ProductModel productModel = new ProductModel();
+            ViewBag.products = productModel.findAll();
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/ProductModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddToCart.Models
+{
+    public class ProductModel
+    {
+        private List<Product> Products;
+
+        public ProductModel()
+        {
+            Products = new List<Product>() {
+                new Product
+                {
+                    Id = "f01",
+                    Name = "Flower 1",
+                    Price = 50,
+                    Photo = "Flower1.PNG"
+                },
+                new Product
+                {
+                    Id = "f02",
+                    Name = "Flower 2",
+                    Price = 80,
+                   Photo = "Flower2.PNG"
+                },
+                new Product
+                {
+                    Id = "f03",
+                    Name = "Flower 3",
+                    Price = 60,
+                    Photo = "Flower3.PNG"
+                },
+                new Product
+                {
+                    Id = "f04",
+                    Name = "Flower 4",
+                    Price = 40,
+                    Photo = "Flower4.PNG"
+                },
+                new Product
+                {
+                    Id = "f05",
+                    Name = "Flower 5",
+                    Price = 70,
+                    Photo = "Flower5.PNG"
+                }
+            };
+        }
+
+        public List<Product> findAll()
+        {
+            return Products;
+        }
+
+        public Product find(string id)
+        {
+            return Products.Where(p => p.Id == id).FirstOrDefault();
+        }
+    }
+}
